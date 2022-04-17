58d4c5fef622cc6bed9afc9c0615e2e426f082e7
saralas18 <54003684+saralas18@users.noreply.github.com>
2022-04-17 15:22:31 +0200

    Travail sur Panier (#1)
    * test
    
    * Avant le panier [WIP]
    
    * Panier [WIP]

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Produit } from '../interfaces/Produit';
+import { PcPortable } from '../pc-portable/pcPortable';
 
 @Component({
   selector: 'app-panier',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./panier.component.css']
 })
 export class PanierComponent implements OnInit {
+  public produitsDansLePanier: Produit[] = [
+{
+  id: 1,
+  caracteristique: "ABC",
+  article: "Spider Man Movie",
+  image: "https://api.lorem.space/image/movie?w=150&h=220&t=1"
+}
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+    // todo: load produitsDansLePanier using the API
+  }
+
+  deleteFromCart(produit: Produit){
+    alert(`${produit.article} a ete suprimee du panier`)
+    //todo: add the backend 
+    // todo: remove produit from produitDansLePanier
   }
 
 }
