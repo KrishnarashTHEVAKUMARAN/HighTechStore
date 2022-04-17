fde70fba369d374d06c6cc9c2573e45a0f7b0af5
Krishnarash <thevakumarankrishnarash@gmail.com>
2022-04-17 16:05:06 +0200

    little fix (reformat code + add one article)

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Produit } from '../interfaces/Produit';
-import { PcPortable } from '../pc-portable/pcPortable';
+import {Component, OnInit} from '@angular/core';
+import {Produit} from '../interfaces/Produit';
+import {PcPortable} from '../pc-portable/pcPortable';
 
 @Component({
   selector: 'app-panier',
@@ -9,21 +9,28 @@ import { PcPortable } from '../pc-portable/pcPortable';
 })
 export class PanierComponent implements OnInit {
   public produitsDansLePanier: Produit[] = [
-{
-  id: 1,
-  caracteristique: "Dimensions 247 x 355 x 19 mm, Poids 2.01 kg, Processeur Intel Core i7-11800H, RAM 16 Go, Carte graphique Nvidia Geforce RTX 3050, Système d'exploitation Windows",
-  article: "Asus Vivobook",
-  image: "assets/image/pcPortable/G_741173_A.avif"
-}
+    {
+      id: 1,
+      caracteristique: "Dimensions 247 x 355 x 19 mm, Poids 2.01 kg, Processeur Intel Core i7-11800H, RAM 16 Go, Carte graphique Nvidia Geforce RTX 3050, Système d'exploitation Windows",
+      article: "Asus Vivobook",
+      image: "assets/image/pcPortable/G_741173_A.avif"
+    },
+    {
+      id: 2,
+      caracteristique: "Capacité du stockage 2 To, Compatibilité du périphérique Console de jeu, Interface du disque dur Serial ATA 600, Marque Seagate",
+      article: "Seagate STBD",
+      image: "assets/image/disqueDur/Seagate_STBD.jpg"
+    }
   ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
     // todo: load produitsDansLePanier using the API
   }
 
-  deleteFromCart(produit: Produit){
+  deleteFromCart(produit: Produit) {
     alert(`${produit.article} a ete suprimee du panier`)
     //todo: add the backend
     // todo: remove produit from produitDansLePanier
